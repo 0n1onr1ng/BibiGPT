74f80e1e321dd03d6bab0e825ebfe6a7eded9423
0n1onr1ng <2092872141@qq.com>
2023-07-16 04:02:26 +0800

    Update shop.tsx

--- pages/shop.tsx
+++ pages/shop.tsx
@@ -12,7 +12,6 @@ export default () => {
           <a href="/wechat.jpg" className="text-green-400 hover:underline" target="_blank" rel="noopener noreferrer">
             「加我微信」
           </a>
-        </div>
       </h2>
       <div className="min-h-screen min-w-fit border-2 border-purple-700">
         <iframe src={CHECKOUT_URL} width="100%" height="1024px"></iframe>
