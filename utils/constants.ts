bdd3449873d2fc927a49affd60bf66bbab8a9d24
0n1onr1ng <2092872141@qq.com>
2023-07-19 15:09:53 +0800

    login

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,5 +1,5 @@
 export const BASE_DOMAIN = 'https://video.aiforce.us'
-export const CHECKOUT_URL = 'https://video.aiforce.us'
+export const CHECKOUT_URL = true
 export const LOGIN_LIMIT_COUNT = 5
 export const FREE_LIMIT_COUNT = 5
 export const RATE_LIMIT_COUNT = LOGIN_LIMIT_COUNT + FREE_LIMIT_COUNT
